17c2a5ad82b5fc10b3c20373301b29c86c7f6af3
fernanadovicente-png <fernanado.vicente@gmail.com>
2025-10-19 20:29:37 +0200

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins:[react()] });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// ⚙️ Corrige o erro do ecrã branco no GitHub Pages
+export default defineConfig({
+  plugins: [react()],
+  base: '/Net_Pro_2/', // 👈 Nome do teu repositório no GitHub
+})
